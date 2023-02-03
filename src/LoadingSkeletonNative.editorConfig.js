015d9bd5ae34cf20b689871af52d224eb19b12c1
Johan Munneke <johanmunneke94@gmail.com>
2023-02-03 17:02:12 +0100

    fixed editorConfig

--- src/LoadingSkeletonNative.editorConfig.js
+++ src/LoadingSkeletonNative.editorConfig.js
@@ -11,6 +11,7 @@ export function getProperties(values, defaultProperties) {
     return defaultProperties;
 }
 
+
 // /**
 //  * @param {Object} values
 //  * @returns {Problem[]} returns a list of problems.
@@ -19,37 +20,6 @@ export function getProperties(values, defaultProperties) {
 //    /** @type {Problem[]} */
 //    const errors = [];
 //    // Add errors to the above array to throw errors in Studio and Studio Pro.
-//    /* Example
-//    if (values.myProperty !== "custom") {
-//        errors.push({
-//            property: `myProperty`,
-//            message: `The value of 'myProperty' is different of 'custom'.`,
-//            url: "https://github.com/myrepo/mywidget"
-//        });
-//    }
-//    */
+   
 //    return errors;
-// }
-
-// /**
-//  * @param {object} values
-//  * @param {boolean} isDarkMode
-//  * @param {number[]} version
-//  * @returns {object}
-//  */
-// export function getPreview(values, isDarkMode, version) {
-//     // Customize your pluggable widget appearance for Studio Pro.
-//     return {
-//         type: "Container",
-//         children: []
-//     };
-// }
-
-// /**
-//  * @param {Object} values
-//  * @param {("web"|"desktop")} platform
-//  * @returns {string}
-//  */
-// export function getCustomCaption(values, platform) {
-//     return "EmptyNativejs";
 // }
